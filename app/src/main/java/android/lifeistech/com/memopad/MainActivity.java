e439b8e44d7fec35515d83686582d192c73aacb7
KentaMasago <masagotty@gmail.com>
2016-06-10 21:29:45 +0900

    check

--- app/src/main/java/android/lifeistech/com/memopad/MainActivity.java
+++ app/src/main/java/android/lifeistech/com/memopad/MainActivity.java
@@ -1,6 +1,7 @@
 package android.lifeistech.com.memopad;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SharedPreferences pref = getSharedPreferences("explain", MODE_PRIVATE);
+        boolean key = pref.getBoolean("key_tutorial", false);
+
+        if (!key) {
+            Intent intent = new Intent(this, ExplainActivity.class);
+            startActivity(intent);
+        }
+
+
     }
 
     public void memo (View v) {
